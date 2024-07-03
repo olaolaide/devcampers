1b70f6f13ea3afc286c4005a1855673cc63b8ac5
Abimbola <olaolaide@outlook.com>
2024-07-03 18:36:36 +0100

    Course Model

---
+++ models/Course.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+
+const CourseSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, 'Please add a name'],
+            trim: true,
+            maxlength: [50, 'Name can not be more than 50 characters']
+        },
+        description: {
+            type: String,
+            required: [true, 'Please add a description'],
+            maxlength: [500, 'Description can not be more than 500 characters']
+        },
+        weeks: {
+            type: String,
+            required: [true, 'Please add number of weeks'],
+        },
+        tuition: {
+            type: Number,
+            required: [true, 'Please add a tuition cost'],
+        },
+        minimumSkill: {
+            type: String,
+            required: [true, 'Please add a tuition cost'],
+            enum: ['beginner', 'intermediate', 'advanced']
+        },
+        scholarshipAvailable: {
+            type: Boolean,
+            default: false
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        bootcamp: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Bootcamp',
+            required: true
+        }
+    }
+);
+
+module.exports = mongoose.model('Course', CourseSchema);
